fc12c8d37ef436b7c9baaee5004997df27d4061d
Lyudmil Lesinski <lyudmilles@gmail.com>
2022-10-11 15:56:11 +0200

    Initial commit

---
+++ lib/nasa_fuel_calculator.ex
@@ -0,0 +1,18 @@
+defmodule NasaFuelCalculator do
+  @moduledoc """
+  Documentation for `NasaFuelCalculator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NasaFuelCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nasa_fuel_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule NasaFuelCalculatorTest do
+  use ExUnit.Case
+  doctest NasaFuelCalculator
+
+  test "greets the world" do
+    assert NasaFuelCalculator.hello() == :world
+  end
+end
