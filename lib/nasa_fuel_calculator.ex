69d8617fd8be0cb79ff196b75834b60c5ce7b458
Lyudmil Lesinski <lyudmilles@gmail.com>
2022-10-17 10:47:24 +0200

    Add fuel calculating module
    Background
    ==========
    I'm adding a module that will calculate the fuel needed for a space
    flight. The function accepts two arguments - the weight of the
    equipment and an array with the mission path. Each step of the mission
    path array must be a tuple in the form `{:launch | :land, float}`.

--- lib/nasa_fuel_calculator.ex
+++ lib/nasa_fuel_calculator.ex
@@ -1,18 +1,36 @@
 defmodule NasaFuelCalculator do
   @moduledoc """
-  Documentation for `NasaFuelCalculator`.
+  This module has functions related to calculating the fuel needed for a space flight which consists of multiple
+  launches and landings.
   """
 
   @doc """
-  Hello world.
+  Calculates the fuel needed for for a given path.
 
   ## Examples
 
-      iex> NasaFuelCalculator.hello()
-      :world
+      iex> NasaFuelCalculator.weight_of_fuel(500, [{:launch, 5.0}, {:land, 4.0}])
+      101
 
   """
-  def hello do
-    :world
+  @type path() :: [{:launch | :land, float()}]
+  @spec weight_of_fuel(integer(), path()) :: integer()
+  def weight_of_fuel(weight_of_equipment, path) do
+    path
+    |> Enum.reverse()
+    |> Enum.reduce(weight_of_equipment, &calculate_fuel_weight/2)
+    |> Kernel.-(weight_of_equipment)
+  end
+
+  defp calculate_fuel_weight(_step, mass) when mass <= 0, do: 0
+
+  defp calculate_fuel_weight({:launch, gravity} = step, mass) do
+    fuel = floor(mass * gravity * 0.042 - 33)
+    mass + calculate_fuel_weight(step, fuel)
+  end
+
+  defp calculate_fuel_weight({:land, gravity} = step, mass) do
+    fuel = floor(mass * gravity * 0.033 - 42)
+    mass + calculate_fuel_weight(step, fuel)
   end
 end
--- test/nasa_fuel_calculator_test.exs
+++ test/nasa_fuel_calculator_test.exs
@@ -2,7 +2,39 @@ defmodule NasaFuelCalculatorTest do
   use ExUnit.Case
   doctest NasaFuelCalculator
 
-  test "greets the world" do
-    assert NasaFuelCalculator.hello() == :world
+  test "Landing Apollo 11" do
+    weight_of_equipment = 28801
+    path = [{:land, 9.807}]
+
+    assert NasaFuelCalculator.weight_of_fuel(weight_of_equipment, path) == 13447
+  end
+
+  test "Apollo 11" do
+    weight_of_equipment = 28801
+    path = [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 9.807}]
+
+    assert NasaFuelCalculator.weight_of_fuel(weight_of_equipment, path) == 51898
+  end
+
+  test "Mission on Mars" do
+    weight_of_equipment = 14606
+    path = [{:launch, 9.807}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]
+
+    assert NasaFuelCalculator.weight_of_fuel(weight_of_equipment, path) == 33388
+  end
+
+  test "Passenger ship" do
+    weight_of_equipment = 75432
+
+    path = [
+      {:launch, 9.807},
+      {:land, 1.62},
+      {:launch, 1.62},
+      {:land, 3.711},
+      {:launch, 3.711},
+      {:land, 9.807}
+    ]
+
+    assert NasaFuelCalculator.weight_of_fuel(weight_of_equipment, path) == 212_161
   end
 end
